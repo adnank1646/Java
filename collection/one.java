1e04b752483d4ed75b80d2f07e620394393be160
Adnan Khan <91988256+adnank1646@users.noreply.github.com>
2024-09-30 20:27:37 +0000

    LISTS

---
+++ collection/one.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class one {
+    public static void main(String[] args) {
+
+
+        List<Integer> list = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        //to add element
+        list.add(10);
+        list.add(52);
+        list.add(1);
+        System.out.println(list);
+        // System.out.println(list.get(0));
+        
+        // list.set(1, 523);
+        // System.out.println(list);
+
+        // list.add(2, 1024);
+        // System.out.println(list);
+
+        // list.remove(1);
+        // System.out.println(list);
+
+        //System.out.println(list.indexOf(1));
+
+
+
+
+        //list2.add(1);
+        //list2.add(2);
+        //list2.add(3);
+
+        //System.out.println(list);
+        // System.out.println(list.size());
+        // System.out.println(list.contains(756));
+
+        // list.remove(1);
+        // list.remove(Integer.valueOf(1));
+
+        //list.addAll(list2);
+        // System.out.println(list);
+        //list.removeAll(list2);
+        //list.retainAll(list2);
+
+        //list.clear();
+        
+        //Object a[] = list.toArray();
+
+        // for(Object e:a){
+        //     Integer temp = (Integer) e;
+        //     System.out.println(e);
+        // }
+
+
+        // System.out.println(list);
+    }
+}
